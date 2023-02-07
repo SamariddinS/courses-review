cdca46c32f40c09de64643eaa476c75443ef344b
Samariddin <92173038@study.bsuir.by>
2023-02-07 19:57:58 +0300

    modified model

---
+++ src/review/review.model.ts
@@ -0,0 +1,21 @@
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+import { prop } from '@typegoose/typegoose/lib/prop';
+import { Types } from 'mongoose';
+
+export interface ReviewModel extends Base {}
+export class ReviewModel extends TimeStamps {
+	@prop()
+	name: string;
+
+	@prop()
+	title: string;
+
+	@prop()
+	description: string;
+
+	@prop()
+	rating: number;
+
+	@prop()
+	productId: Types.ObjectId;
+}
